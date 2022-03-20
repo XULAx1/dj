885af682fb6991496ab1c7d54724b4d1fcf820b0
XULAx1 <98847120+XULAx1@users.noreply.github.com>
2022-03-20 13:49:53 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const db = require('quick.db');
 const { TOKEN, PREFIX, AVATARURL, BOTNAME, } = require(`./config.json`);
 const figlet = require("figlet");
 const client = new Client({ disableMentions: `` , partials: ['MESSAGE', 'CHANNEL', 'REACTION'] });
-client.login('Nzg0MzAzNzU2OTI1NDY4NzEz.X8nVyw.eSrDPfzRnwffCXIT3UbiV_DQKTc');
+client.login('OTU0NTI3OTkyNDgxMTk4MDgw.YjUbdw.yJmb3FGCJ-8n69h5d7LkB9dBPFI');
 client.commands = new Collection();
 client.setMaxListeners(0);
 client.prefix = PREFIX;
